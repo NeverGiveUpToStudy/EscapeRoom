26dc288586483bfc9f0d384bc8a3e32fdafd8109
NeverGiveUpToStudy <wuliangjun007@qq.com>
2022-08-02 12:04:03 +0800

    给幽灵添加自动寻路

---
+++ Assets/Scripts/WaypointPatrol.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WaypointPatrol : MonoBehaviour {
+
+    public NavMeshAgent navMeshAgent;
+
+    public Transform[] waypoints;
+    private int m_CurrentWaypointIndex;
+
+    private void Start() {
+        navMeshAgent.SetDestination(waypoints[0].position);
+    }
+
+    private void Update() {
+        if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance) {
+            m_CurrentWaypointIndex = (waypoints.Length + 1) % waypoints.Length;
+            navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
+        }
+    }
+}
